528bff10023725e3688c918e03b6bdb1da0f226b
R <r@localhost.localdomain>
2022-07-06 16:00:30 -0500

    options added

--- single_images.py
+++ single_images.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     """Main Stuff"""
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file", required = True, help = "select filepath of the image")
-    parser.add_argument("-s", "--style", required True, help = "select art style in options.txt")
+    parser.add_argument("-s", "--style", required = True, help = "select art style in options.txt")
     parser.add_argument("-o", "--output", required = True, help = "select output file")
     args = parser.parse_args()
     get_art_image(args.file , args.style, args.output)
