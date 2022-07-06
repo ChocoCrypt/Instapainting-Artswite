42711d3c9127ae0e397e4cbb9fc1602f94efd919
R <r@localhost.localdomain>
2022-07-06 15:58:48 -0500

    method for single file added

--- convert_image.py
+++
@@ -1,5 +0,0 @@
-from method-upload_file import get-art_image
-import argparse
-
-
-
---
+++ single_images.py
@@ -0,0 +1,14 @@
+"""Interface for working with single images"""
+from method_upload_file import get_art_image
+import argparse
+
+
+
+if __name__ == "__main__":
+    """Main Stuff"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", required = True, help = "select filepath of the image")
+    parser.add_argument("-s", "--style", required True, help = "select art style in options.txt")
+    parser.add_argument("-o", "--output", required = True, help = "select output file")
+    args = parser.parse_args()
+    get_art_image(args.file , args.style, args.output)
