d8c70873d8305e9a256f1495bec8378cdac64a4b
R <r@localhost.localdomain>
2022-07-06 22:59:30 -0500

    Video converted is now working!

--- method_upload_file.py
+++ method_upload_file.py
@@ -17,44 +17,49 @@ def get_art_image(img_path , art_form , output):
     Uploads a file with requests and then apply the filter with selenium and
     download the result given by instapainting.
     """
-    # Driver Options
-    options = webdriver.ChromeOptions()
-    #options.add_argument("--headless")
-    # Login stuff
-    driver = webdriver.Chrome(options = options)
-    driver.get("https://www.instapainting.com/assets")
-    driver.implicitly_wait(10)
-    # Uploading image to instapainting
-    upload_photo_box = driver.find_element_by_class_name("c1kjkl90")
-    upload_photo_box.send_keys(os.getcwd()+img_path)
-    # Configuration of the requests session for uploading the file
-    s = requests.Session()
-    cookies = driver.get_cookies()
-    for cookie in cookies:
-        s.cookies.set(cookie['name'], cookie['value'])
-    # Uploading the file with requests
-    files = {'file': open(img_path, 'rb')}
-    s = requests.post("https://www.instapainting.com/updates/create-chunked/", files=files)
-    # Formating recived data to get the id of the photo
-    recived_data = json.loads(s.content)
-    page_id = recived_data["result"]["id"]
-    # Going to the page of the uploaded file
-    driver.get(f"https://www.instapainting.com/assets/{page_id}")
-    driver.implicitly_wait(600)
-    # Updating options
-    all_art_forms = driver.find_elements_by_class_name("_14xSf7HUhOgutHdGN--E87")
-    options_txt = "\n".join([i.text for i in all_art_forms])
-    with open("options.txt","w") as opt:
-        opt.write(options_txt)
-    # Select Art form in possible art forms in options.txt
-    art_box = driver.find_element_by_link_text(art_form)
-    art_box.click()
-    # Downloading images
-    resulting_image = driver.find_element_by_class_name("_3FAK3-36q_B8HP2pGt6GJG")
-    resulting_image_url = resulting_image.get_attribute("src")
-    result = requests.get(resulting_image_url).content
-    # Saving results
-    with open(output, "wb") as rs:
-        rs.write(result)
-    # Return result only if it's necesary
-    return(result)
+    try:
+        # Driver Options
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        # Login stuff
+        driver = webdriver.Chrome(options = options)
+        driver.get("https://www.instapainting.com/assets")
+        driver.implicitly_wait(10)
+        # Uploading image to instapainting
+        upload_photo_box = driver.find_element_by_class_name("c1kjkl90")
+        upload_photo_box.send_keys(os.getcwd()+img_path)
+        # Configuration of the requests session for uploading the file
+        s = requests.Session()
+        cookies = driver.get_cookies()
+        for cookie in cookies:
+            s.cookies.set(cookie['name'], cookie['value'])
+        # Uploading the file with requests
+        files = {'file': open(img_path, 'rb')}
+        s = requests.post("https://www.instapainting.com/updates/create-chunked/", files=files)
+        # Formating recived data to get the id of the photo
+        recived_data = json.loads(s.content)
+        page_id = recived_data["result"]["id"]
+        # Going to the page of the uploaded file
+        driver.get(f"https://www.instapainting.com/assets/{page_id}")
+        driver.implicitly_wait(600)
+        # Updating options
+        all_art_forms = driver.find_elements_by_class_name("_14xSf7HUhOgutHdGN--E87")
+        options_txt = "\n".join([i.text for i in all_art_forms])
+        with open("options.txt","w") as opt:
+            opt.write(options_txt)
+        # Select Art form in possible art forms in options.txt
+        art_box = driver.find_element_by_link_text(art_form)
+        art_box.click()
+        # Downloading images
+        resulting_image = driver.find_element_by_class_name("_3FAK3-36q_B8HP2pGt6GJG")
+        resulting_image_url = resulting_image.get_attribute("src")
+        result = requests.get(resulting_image_url).content
+        # Saving results
+        with open(output, "wb") as rs:
+            rs.write(result)
+        # Return result only if it's necesary
+        return(result)
+    except:
+        pass
+    finally:
+        driver.close()
--- video_converter.py
+++ video_converter.py
@@ -1,7 +1,12 @@
 """Tool for creating a video using art CNN"""
+#import moviepy.video.io.ImageSequenceClip
+import ffmpeg
 import tempfile
 import hashlib 
 import os
+from method_upload_file import get_art_image
+from tqdm import tqdm  
+import argparse
 
 
 # This method is not being used because is easy to create a temporary file
@@ -17,18 +22,41 @@ def name_directory(video_path):
     return(hashed)
 
 
-def extract_video_frames(path , frames_per_second):
+def extract_video_frames(path , frames_per_second , art_type , output):
     """
     Extracting video frames into a temporary file.
     """
     # Creating a temporary directory
     with tempfile.TemporaryDirectory() as tmpdirname: # Temporary file is names tmpdirname
         # Converting video to frames with ffmpeg
-        command = f"ffmpeg -i {path}  -vf fps=1/60 img%03d.jpg"
+        command = f"ffmpeg -i {path}  -vf fps={frames_per_second} {tmpdirname}/img%08d.jpg"
+        print(tmpdirname)
         os.system(command)
+        # Getting Images collected
+        images_path = [f"{tmpdirname}/{i}" for i in os.listdir(tmpdirname)]
+        # Converting images to art_stuff - Had to create another temporary
+        # directory to store the art files into it
+        with tempfile.TemporaryDirectory() as tmpdir2:
+            # Converting every single image to art
+            for j,i in (enumerate(tqdm(images_path))):
+                get_art_image(i, art_type , f"{tmpdir2}/image{j:08d}.jpg")
+            # Collapsing folder into new video
+            os.system(f"ls {tmpdir2}")
+            command = f'ffmpeg -framerate {frames_per_second} -pattern_type glob -i "{tmpdir2}/*.jpg" {output} > /dev/null 2>&1'
+            print(command)
+            os.system(command)
+            print("done")
+
+
 
         
 
-extract_video_frames("video.mp4" , 10)
-
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f" , "--file" , required = True , help = "Video file")
+    parser.add_argument("-fps" "--fps" , required = True , help = "Frames per second")
+    parser.add_argument("-s" , "--style" , required = True , help = "Style on options.txt")
+    parser.add_argument("-o" , "--output" , requited = True , help = "output")
+    args = parser.parse_args()
+    extract_video_frames(args.file , args.fps , args.style, args.output)
 
